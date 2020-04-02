ee54d94225d219661ee658b974301b5f10786baa
Victor Didenko <victord@setplex.com>
2020-04-02 14:24:48 +0300

    Add tests

--- test/01_counter.js
+++ test/01_counter.js
@@ -1,26 +1,47 @@
+import ninos from 'ninos'
+import test from 'ava'
 import { createStore, createEvent } from '../src/index.js'
 
-const add = createEvent()
-const sub = createEvent()
-const reset = createEvent()
+const it = ninos(test)
 
-const counter = createStore(0)
-  .on(add, (count, n) => count + n)
-  .on(sub, (count, n) => count - n)
-  .reset(reset)
+it('#1: Counter should work as expected', t => {
+  const log = t.context.stub()
 
-counter.watch(n => console.log('counter:', n))
-// counter: 0
-add.watch(n => console.log('add', n))
-sub.watch(n => console.log('subtract', n))
-reset.watch(() => console.log('reset counter'))
+  const add = createEvent()
+  const sub = createEvent()
+  const reset = createEvent()
 
-add(5)
-// add 5
-// counter: 5
-sub(1)
-// subtract 1
-// counter: 4
-reset()
-// reset counter
-// counter: 0
+  const counter = createStore(0)
+    .on(add, (count, n) => count + n)
+    .on(sub, (count, n) => count - n)
+    .reset(reset)
+
+  counter.watch(n => log('counter:', n))
+  // counter: 0
+  add.watch(n => log('add', n))
+  sub.watch(n => log('subtract', n))
+  reset.watch(() => log('reset counter'))
+
+  add(5)
+  // add 5
+  // counter: 5
+  sub(1)
+  // subtract 1
+  // counter: 4
+  reset()
+  // reset counter
+  // counter: 0
+
+  t.deepEqual(
+    log.calls.map(c => c.arguments),
+    [
+      ['counter:', 0],
+      ['add', 5],
+      ['counter:', 5],
+      ['subtract', 1],
+      ['counter:', 4],
+      ['reset counter'],
+      ['counter:', 0],
+    ]
+  )
+})
